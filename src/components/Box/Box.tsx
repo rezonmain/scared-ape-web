10aabdccbb94ee9babb9d1a54003d93551c57c66
rezonmain <adiazgomez101@gmail.com>
2023-07-04 16:43:49 -0700

    fix(Box): fix required className prop

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -3,11 +3,11 @@ const Box = ({
   className,
 }: {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 }) => {
   return (
     <div
-      className={`p-4 my-8 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ${className} `}
+      className={`p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ${className} `}
     >
       {children}
     </div>
