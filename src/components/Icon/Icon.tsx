4904438f62f2fead9d22d22cc28b8e75d0a1f36a
rezonmain <adiazgomez101@gmail.com>
2023-07-04 20:06:04 -0700

    feat(Icon): add burger menu icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -141,6 +141,21 @@ const icons = {
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
     </svg>
   ),
+  burger: (
+    <svg
+      className="w-6 h-6"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  ),
 };
 
 const Icon: FC<IconProps> = (props: IconProps) => {
