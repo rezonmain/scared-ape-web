eb35e701425724906b6afefb5c769e6126bb40b2
rezonmain <adiazgomez101@gmail.com>
2023-07-04 22:19:32 -0700

    feat(dashboard): add dashboard page

---
+++ src/routes/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { useSearchParams } from "react-router-dom";
+import { useStore } from "@/context/app/app.context";
+import { useEffect } from "react";
+import { Validator } from "@/utils/Validate";
+
+export function DashboardPage() {
+  const [params] = useSearchParams();
+  const { dispatch, user } = useStore();
+
+  useEffect(() => {
+    if (!params.has("cuid")) return;
+    const validator = new Validator();
+    const user = validator.gerUserFromQueryParams(params);
+    dispatch({ type: "set_user", payload: user });
+  }, [dispatch, params]);
+
+  return (
+    <div className="mx-auto h-screen flex items-center justify-center flex-col max-w-sm">
+      <pre className="text-white">
+        <code>{JSON.stringify(user, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
