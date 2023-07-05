d53cb4fa431a3de439f28662145a0ceba0418013
rezonmain <adiazgomez101@gmail.com>
2023-07-04 22:17:56 -0700

    feat(appContext): add user to reducer

--- src/context/app/app.reducer.ts
+++ src/context/app/app.reducer.ts
@@ -1,14 +1,18 @@
 import { AppStore } from "./app.context";
 
-export type StoreAction = {
-  type: "set_drawer";
-  payload: AppStore["drawer"];
-};
+export type StoreAction =
+  | {
+      type: "set_drawer";
+      payload: AppStore["drawer"];
+    }
+  | { type: "set_user"; payload: AppStore["user"] };
 
 const appReducer = (state: AppStore, action: StoreAction): AppStore => {
   switch (action.type) {
     case "set_drawer":
       return { ...state, drawer: action.payload };
+    case "set_user":
+      return { ...state, user: action.payload };
     default:
       return state;
   }
