8d509e08b1316a9989b089a74cba8b440766e88f
rezonmain <adiazgomez101@gmail.com>
2023-07-09 21:25:23 -0700

    feat(Button): add disable style

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,11 +4,11 @@ interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
   color?: "primary" | "secondary";
 }
 
-const Button = (props: ButtonProps) => {
+const Button = ({ className, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:bg-blue-400 disabled:dark:bg-blue-500 disabled:cursor-not-allowed ${className}`}
     >
       {props.children}
     </button>
