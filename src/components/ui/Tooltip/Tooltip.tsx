fa92aa9a7f78322237634c7526528e22085125ac
rezonmain <adiazgomez101@gmail.com>
2023-07-09 22:19:36 -0700

    feat(Tooltip): update tooltip comp

--- src/components/ui/Tooltip/Tooltip.tsx
+++ src/components/ui/Tooltip/Tooltip.tsx
@@ -1,28 +1,40 @@
 import { useFlowbiteTooltip } from "@/hooks/useFlowbiteTooltip";
 import { useRef } from "react";
 
-interface TooltipProps {
+export interface TooltipProps {
+  show?: boolean;
   children: React.ReactNode;
-  text: string;
+  text?: string;
   placement?: "top" | "bottom" | "left" | "right";
 }
 
-const Tooltip = ({ children, text, placement = "top" }: TooltipProps) => {
+const Tooltip = ({
+  children,
+  text,
+  placement = "top",
+  show = true,
+}: TooltipProps) => {
   const targetRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLDivElement>(null);
   useFlowbiteTooltip(targetRef, triggerRef, { placement });
 
   return (
     <>
-      <div ref={triggerRef}>{children}</div>
-      <div
-        ref={targetRef}
-        role="tooltip"
-        className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-      >
-        {text}
-        <div className="tooltip-arrow" data-popper-arrow></div>
-      </div>
+      {show ? (
+        <>
+          <div ref={triggerRef}>{children}</div>
+          <div
+            ref={targetRef}
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            {text ?? ""}
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        </>
+      ) : (
+        <>{children}</>
+      )}
     </>
   );
 };
