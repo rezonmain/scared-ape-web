fec41f0166b424f9b9fa5d0a8c64de8714f7c640
rezonmain <adiazgomez101@gmail.com>
2023-07-18 19:48:05 -0700

    fix(scraperCard): style

--- src/components/ScraperCard/ScraperCard.tsx
+++ src/components/ScraperCard/ScraperCard.tsx
@@ -12,7 +12,7 @@ interface ScraperCardProps {
 const ScraperCard = ({ scraper }: ScraperCardProps) => {
   const { user } = useAppStore();
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="format max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {scraper.name}
       </h5>
