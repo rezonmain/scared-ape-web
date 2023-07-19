23275866778788fc689d52304463fb684d282f23
rezonmain <adiazgomez101@gmail.com>
2023-07-18 19:46:13 -0700

    feat(ui): add PageLoad

---
+++ src/components/ui/PageLoad/PageLoad.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "flowbite-react";
+
+const PageLoad = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <Spinner />
+    </div>
+  );
+};
+
+export { PageLoad };
