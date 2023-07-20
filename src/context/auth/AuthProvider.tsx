03aff18b1d497325352b9313038ce56b85e6971e
rezonmain <adiazgomez101@gmail.com>
2023-07-19 23:06:07 -0700

    feat(authContext): initialize AuthProvider with auth instance

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -1,14 +1,14 @@
 import { ReactNode, useReducer } from "react";
 import { AuthContext } from "./auth.context";
 import { authReducer } from "./auth.reducer";
-import { defualtState } from "./auth.store";
+import { auth } from "@/services/Auth";
 
 export const AuthProvider = ({
   children,
 }: {
   children: ReactNode;
 }): JSX.Element => {
-  const [init, dispatch] = useReducer(authReducer, defualtState);
+  const [init, dispatch] = useReducer(authReducer, auth());
   return (
     <AuthContext.Provider value={{ ...init, dispatch }}>
       {children}
