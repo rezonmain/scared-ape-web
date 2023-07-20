75b2c1fa3f7dae0386296db3fb8efe595b320aa6
rezonmain <adiazgomez101@gmail.com>
2023-07-19 23:05:47 -0700

    feat(app): remove AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./router";
-import { AppProvider } from "./context/app/app.provider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
+    <RouterProvider router={router} />
     <ToastContainer />
   </React.StrictMode>
 );
