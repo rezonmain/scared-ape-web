38c7a2fce968f4efd63a3b63e30522433f8f6962
rezonmain <adiazgomez101@gmail.com>
2023-07-19 23:26:53 -0700

    feat(app): removed AuthProvider

--- src/layouts/global.tsx
+++ src/layouts/global.tsx
@@ -1,9 +1,8 @@
-import { AuthProvider } from "@/context/auth/AuthProvider";
 import { useToastFromQuery } from "@/hooks/useToastFromQuery";
 
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   useToastFromQuery();
-  return <AuthProvider>{children}</AuthProvider>;
+  return <>{children};</>;
 };
 
 export { GlobalLayout };
