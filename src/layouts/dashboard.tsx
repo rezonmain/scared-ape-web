146786a5b72ccd7d9ab4dcf183508290d7bf8193
rezonmain <adiazgomez101@gmail.com>
2023-07-22 01:29:48 -0700

    feat(dashboardLayout): made dashboard a protected layout

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,11 +1,28 @@
 import { Nav } from "@/components/ui/Nav/Nav";
 import { Sidebar } from "@/components/ui/Sidebar/Sidebar";
+import { useAuth } from "@/hooks/useAuth";
 import { useFlowbiteDrawer } from "@/hooks/useFlowbiteDrawer";
-import { ComponentProps, useRef } from "react";
+import { QueryAlert } from "@/types/QueryAlert";
+import { ComponentProps, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const sidebarRef = useRef<HTMLDivElement>(null);
   const { drawer } = useFlowbiteDrawer(sidebarRef);
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuth) {
+      const { params } = new QueryAlert({
+        title: "Not signed in",
+        message:
+          "Either your session expired or you navigated to a page by accident, please log in",
+        type: "info",
+      });
+      navigate(`/auth?${params}`);
+    }
+  }, [isAuth]);
 
   const items: ComponentProps<typeof Sidebar>["items"] = [
     {
