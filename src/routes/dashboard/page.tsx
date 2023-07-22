96809abac04761e577dda02f853d281e624f7658
rezonmain <adiazgomez101@gmail.com>
2023-07-21 21:42:40 -0700

    feat(dashboard): remove dashboard route

--- src/routes/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export function DashboardPage() {
-  return (
-    <div className="mx-auto h-screen flex items-center justify-center flex-col max-w-sm">
-      <pre className="text-white"></pre>
-    </div>
-  );
-}
