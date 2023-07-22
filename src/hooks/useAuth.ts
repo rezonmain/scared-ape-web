333a1d1bb2a08cc9dcce6ab7aaf469148bef4a4b
rezonmain <adiazgomez101@gmail.com>
2023-07-21 21:14:27 -0700

    feat(useAuthHook): add set user method

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,10 +3,13 @@ import { auth } from "@/services/Auth";
 import { useCallback, useMemo, useRef } from "react";
 
 const useAuth = () => {
-  const authRef = useRef(auth());
+  const authRef = useRef(auth);
   const logout = useCallback(authRef.current.logout, []);
   const fetchUser = useCallback(authRef.current.fetchUser, []);
-  const setUser = useCallback((user: User | null) => (auth().user = user), []);
+  const setUser = useCallback(
+    (user: User | null) => (authRef.current.user = user),
+    []
+  );
   const user = useMemo(() => authRef.current.user, [authRef.current.user]);
   return { user, logout, fetchUser, setUser };
 };
