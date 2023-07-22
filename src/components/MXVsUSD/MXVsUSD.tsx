113bf089bbec55f923403bb914ec52e5301a6d2b
rezonmain <adiazgomez101@gmail.com>
2023-07-22 01:29:32 -0700

    feat(MXVsUSD): spinner

--- src/components/MXVsUSD/MXVsUSD.tsx
+++ src/components/MXVsUSD/MXVsUSD.tsx
@@ -1,12 +1,13 @@
 import { useWidgetData } from "@/hooks/useWidgetData";
+import { Spinner } from "flowbite-react";
 
 const MXVsUSD = () => {
   const { json, isLoading } = useWidgetData("mx-vs-usd");
 
   return (
     <div>
-      {isLoading && <div>Loading...</div>}
-      <pre>{JSON.stringify(json, null, 2)}</pre>
+      {isLoading && <Spinner />}
+      {json && <pre>{JSON.stringify(json, null, 2)}</pre>}
     </div>
   );
 };
