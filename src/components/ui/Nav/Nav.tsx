45a9d2cb347bdfc510ae947725375c81daa85bc7
rezonmain <adiazgomez101@gmail.com>
2023-07-27 21:10:43 -0700

    fix(Nav): breakpoints

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -25,15 +25,15 @@ const Nav = (props: NavProps) => {
         <button
           type="button"
           onClick={() => drawer?.show()}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="drawer-navigation"
           aria-expanded="false"
         >
           <span className="sr-only">Open main menu</span>
           <Icon name="burger" />
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-          <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="hidden w-full lg:block lg:w-auto" id="navbar-dropdown">
+          <ul className="flex flex-col items-center font-medium p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
             {items.map((item) => (
               <NavItem key={item.label} {...item} />
             ))}
