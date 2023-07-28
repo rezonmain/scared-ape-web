ff70b4cf5baa826412ad7376ac06742259b64587
rezonmain <adiazgomez101@gmail.com>
2023-07-27 23:12:03 -0700

    feat(ui): add AccessRequestList

---
+++ src/components/AccessRequestList/AccessRequestList.tsx
@@ -0,0 +1,10 @@
+import { AccessRequest } from "@/models/AccessRequest";
+
+type AccessRequestProps = {
+  list: AccessRequest[];
+};
+const AccessRequestList = ({ list }: AccessRequestProps) => {
+  return <></>;
+};
+
+export { AccessRequestList };
