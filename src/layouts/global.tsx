0126218a40ae9fcbab5b1971c407f4a6c0d244ff
rezonmain <adiazgomez101@gmail.com>
2023-07-27 21:10:33 -0700

    feat(globalLayout): add footer

--- src/layouts/global.tsx
+++ src/layouts/global.tsx
@@ -1,8 +1,14 @@
+import { Footer } from "@/components/ui/Footer/Footer";
 import { useToastFromQuery } from "@/hooks/useToastFromQuery";
 
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   useToastFromQuery();
-  return <>{children};</>;
+  return (
+    <div className="h-screen flex flex-col gap-8">
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export { GlobalLayout };
