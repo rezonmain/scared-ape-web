678e3a62d740c8e3afbd4b4626cf4461beaa034c
rezonmain <adiazgomez101@gmail.com>
2023-07-29 10:11:23 -0700

    feat(models): update entity

--- src/models/Entity.ts
+++ src/models/Entity.ts
@@ -1,5 +1,5 @@
 export interface Entity {
   readonly id?: number | bigint;
-  readonly createdAt?: Date;
-  readonly updatedAt?: Date;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
 }
