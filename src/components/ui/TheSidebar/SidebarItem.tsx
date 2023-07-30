c3f45b394b2d181d742a703a65e6cea30a41150e
rezonmain <adiazgomez101@gmail.com>
2023-07-30 13:00:03 -0700

    feat(Sidebar): move to TheSidebar, add drawer back

---
+++ src/components/ui/TheSidebar/SidebarItem.tsx
@@ -0,0 +1,59 @@
+import { ComponentProps } from "react";
+import { Icon } from "../Icon/Icon";
+import { Link } from "react-router-dom";
+import { isNothing } from "@/utils/_";
+import { DrawerInterface } from "flowbite";
+
+type SidebarItemProps = {
+  href: string;
+  icon?: ComponentProps<typeof Icon>["name"];
+  label: string;
+  items?: Omit<SidebarItemProps, "icon" | "items">[];
+  drawer?: DrawerInterface;
+};
+
+const SidebarItem = (props: SidebarItemProps) => {
+  if (!isNothing(props.items)) {
+    return (
+      <>
+        <button
+          type="button"
+          className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          aria-controls={`dropdown-${props.label}`}
+          data-collapse-toggle={`dropdown-${props.label}`}
+        >
+          {props.icon && <Icon name={props.icon} />}
+          <span className="flex-1 ml-3 text-left whitespace-nowrap">
+            {props.label}
+          </span>
+          <Icon name="chevronDown" />
+        </button>
+        <ul id={`dropdown-${props.label}`} className="hidden py-2 space-y-2">
+          {props?.items?.map((item) => (
+            <li>
+              <Link
+                onClick={() => props.drawer?.hide()}
+                to={item.href}
+                className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  return (
+    <Link
+      to={props.href}
+      onClick={() => props.drawer?.hide()}
+      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+    >
+      {props.icon && <Icon name={props.icon} />}
+      <span className="ml-3">{props.label}</span>
+    </Link>
+  );
+};
+
+export { SidebarItem };
