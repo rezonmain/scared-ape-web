d849d6e95079a64864c7cac6f65c3af20dea04ad
rezonmain <adiazgomez101@gmail.com>
2023-07-30 13:22:46 -0700

    feat(floatingLayout): padding

--- src/layouts/floating.tsx
+++ src/layouts/floating.tsx
@@ -2,7 +2,7 @@ import { Footer } from "@/components/ui/TheFooter/Footer";
 
 const FloatingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex flex-col gap-8">
+    <div className="h-screen flex flex-col gap-8 p-4">
       <article className="flex-1">{children}</article>
       <Footer />
     </div>
