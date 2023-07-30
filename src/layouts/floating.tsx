bb40c6476f6dfb9ba561d45628ad02fd12bd4580
rezonmain <adiazgomez101@gmail.com>
2023-07-30 12:59:15 -0700

    feat(layouts): add floating layout for not dashboard pages

---
+++ src/layouts/floating.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/ui/TheFooter/Footer";
+
+const FloatingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen flex flex-col gap-8">
+      <article className="flex-1">{children}</article>
+      <Footer />
+    </div>
+  );
+};
+
+export { FloatingLayout };
