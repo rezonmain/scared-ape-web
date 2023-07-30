504bcc51d96b2676cdc81144e2215f672c3c67cb
rezonmain <adiazgomez101@gmail.com>
2023-07-30 13:01:11 -0700

    feat(Breadcrumb): move to TheBreadcrumb

---
+++ src/components/ui/TheBreadcrumb/Breadcrumb.tsx
@@ -0,0 +1,41 @@
+import { useBreadcrumb } from "@/hooks/useBreadcrumb";
+import { Link } from "react-router-dom";
+import { startCase } from "lodash";
+import { Icon } from "../Icon/Icon";
+
+const Breadcrumb = () => {
+  const { crumbs } = useBreadcrumb();
+  return (
+    <div
+      className="flex max-w-screen-xl flex-wrap items-center justify-between p-4"
+      aria-label="Breadcrumb"
+    >
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <Link
+            to="/"
+            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+          >
+            <Icon name="crumbHome" />
+            Home
+          </Link>
+        </li>
+        {crumbs.map((crumb) => (
+          <li>
+            <div className="flex items-center">
+              <Icon name="crumbChevron" />
+              <Link
+                to={crumb.href}
+                className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+              >
+                {startCase(crumb.pathname)}
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export { Breadcrumb };
