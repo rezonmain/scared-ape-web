32a58f6b2dc662891423fe804a60e52b2278eaea
rezonmain <adiazgomez101@gmail.com>
2023-07-30 13:49:21 -0700

    feat(Breadcrumb): style

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -14,7 +14,7 @@ const Nav = (props: NavProps) => {
       <button
         type="button"
         onClick={() => drawer?.show()}
-        className="inline-flex items-center p-2 w-10 h-10 lg:hidden justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="inline-flex items-center w-10 h-10 lg:hidden justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="drawer-navigation"
         aria-expanded="false"
       >
