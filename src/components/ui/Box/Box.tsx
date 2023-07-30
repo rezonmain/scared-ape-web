31aae6f1be51c14abd403eb40a5f59ebabc43127
rezonmain <adiazgomez101@gmail.com>
2023-07-30 16:38:29 -0700

    feat(UI): extend box component

--- src/components/ui/Box/Box.tsx
+++ src/components/ui/Box/Box.tsx
@@ -1,17 +1,19 @@
-const Box = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
+import { PropsWithChildren, forwardRef } from "react";
+
+const Box = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<{ className: string }>
+>((props, ref) => {
+  const { children, className, ...rest } = props;
   return (
     <div
-      className={`p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ${className} `}
+      {...rest}
+      ref={ref}
+      className={`p-4 bg-white border border-black rounded-lg md:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ${className} `}
     >
       {children}
     </div>
   );
-};
+});
 
 export { Box };
