cfd08eb63a858730c7de5925be7a42a25f613694
rezonmain <adiazgomez101@gmail.com>
2023-07-30 16:38:46 -0700

    feat(UI): add Indicator component

---
+++ src/components/ui/Indicator/Indicator.tsx
@@ -0,0 +1,10 @@
+const Indicator = () => {
+  return (
+    <span className="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+      <span className="w-2 h-2 mr-1 bg-green-500 rounded-full"></span>
+      Latest
+    </span>
+  );
+};
+
+export { Indicator };
