041f6ac16fbfdfbe15f73037a25818c3f5f5d666
rezonmain <adiazgomez101@gmail.com>
2023-07-30 16:39:09 -0700

    fix(sidebar): style

--- src/components/ui/TheSidebar/Sidebar.tsx
+++ src/components/ui/TheSidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>((props, ref) => {
     <div
       ref={ref}
       id="drawer-navigation"
-      className="fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white dark:bg-gray-800 lg:translate-x-0 lg:sticky lg:inset-0 lg:min-h-full"
+      className="fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white border-gray-200 dark:bg-gray-800 lg:translate-x-0 lg:sticky lg:inset-0 lg:min-h-full"
       tabIndex={-1}
     >
       <h5
@@ -30,7 +30,7 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>((props, ref) => {
         type="button"
         onClick={() => drawer?.hide()}
         aria-controls="drawer-navigation"
-        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        className="text-gray-400 bg-transparent lg:hidden hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
       >
         <Icon name="close" />
         <span className="sr-only">Close menu</span>
