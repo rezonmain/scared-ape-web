81a17f459123b228e7fe53581c2a870ff391c688
rezonmain <adiazgomez101@gmail.com>
2023-07-30 17:03:08 -0700

    fix(PaginatedTable): incorrect context type

--- src/components/ui/PaginatedTable/PaginatedTable.context.ts
+++ src/components/ui/PaginatedTable/PaginatedTable.context.ts
@@ -5,11 +5,15 @@ const PaginatedTableContext = createContext<PaginatedTableProps<any>>({
   data: [],
   selectable: false,
   pagination: {
-    currentPage: 0,
-    nextPage: null,
-    previouPage: null,
-    totalPages: 0,
-    totalRecords: 0,
+    position: "bottom",
+    data: {
+      currentPage: 1,
+      totalPages: 1,
+      nextPage: null,
+      previousPage: null,
+      pageSize: 10,
+      totalRecords: 0,
+    },
   },
 });
 
